f29dabb9806a25e3d598ecae69476951e5ab8f53
nsinghop <fakehu45@gmail.com>
2025-10-19 22:09:18 +0530

    Update payment.js

--- src/payment.js
+++ src/payment.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://hireme.codesingh.in/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
